67d3c4d4e847b53b9668634c272eac6e74d454e9
Ananya jantli <jantliananya@gmail.com>
2025-11-27 15:38:05 +0530

    Create electricity_bill.py

---
+++ electricity_bill.py
@@ -0,0 +1,20 @@
+
+import sys
+
+RATE_PER_UNIT = 5.0  
+
+if len(sys.argv) == 2:
+    script_name = sys.argv[0]
+    units = float(sys.argv[1])
+    print("User provided input units:")
+else:
+    script_name = sys.argv[0]
+    units = 100.0
+    print("No input given – using default units (100):")
+
+bill_amount = units * RATE_PER_UNIT
+
+print("Script Name:", script_name)
+print("Units consumed:", units)
+print("Rate per unit (₹):", RATE_PER_UNIT)
+print("Total electricity bill (₹):", bill_amount)
